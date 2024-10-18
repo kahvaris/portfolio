8f6505f2a815b4b942bfcfddc56b3052df0cbb76
variskay <kerttu.a.h.varis@student.jyu.fi>
2024-10-18 14:19:00 +0300

    navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import React, { useState } from 'react';
 
 //Theme
@@ -7,13 +6,8 @@ import { ThemeProvider } from 'styled-components';
 import { Theme1, Theme2, Theme3 } from './themes';
 
 //Components
-import Button from './components/button';
 import Screen from './components/screen';
 import BackG from './components/background';
-
-import Home from './pages/Home';
-import Education from './pages/Education';
-import Portfolio from './pages/Portfolio';
 
 
 const App = () => {
@@ -33,43 +27,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       {/* Optional Global styles */}
       <BackG>
-        
         <Screen>
-          <div style={{ position: 'fixed', top: '0', width: '100px', height: '50px', backgroundColor: 'yellow'}}>
-          <button onClick={toggleTheme1} id='T1'></button>
-          <button onClick={toggleTheme2} id='T2'></button>
-          <button onClick={toggleTheme3} id='T3'></button>
-          </div>
-          <div style={{ position: 'inherit', top: '50px', padding: '30px', width: 'auto', height: 'auto', backgroundColor: 'beige' }}>
-            {/*<Button>Click Me</Button>*/}
-            <Router>
-            <div>
-              {/* Navigation Bar */}
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/education">Education</Link>
-                  </li>
-                  <li>
-                    <Link to="/portfolio">Portfolio</Link>
-                  </li>
-                </ul>
-              </nav>
-
-              {/* Define Routes */}
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/education" element={<Education />} />
-                <Route path="/portfolio" element={<Portfolio />} />
-              </Routes>
-            </div>
-          </Router>
-      
-
-          </div>
         </Screen>
       </BackG>
     </ThemeProvider>
--- src/components/screen.js
+++ src/components/screen.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 //Theme
 import { ThemeProvider } from 'styled-components';
 import { Theme1, Theme2, Theme3 } from '../themes';
+
+import Home from '../pages/Home';
+import Education from '../pages/Education';
+import Portfolio from '../pages/Portfolio';
 
 const StyledScreen = styled.div`
   background-color: white;
@@ -23,6 +28,27 @@ const StyledScreen = styled.div`
 
 `;
 
+const ThemeNav = styled.div`
+  position: sticky;
+  top: 0;
+  width: auto;
+  height: 50px;
+  background-color: white;
+  z-index: 3;
+  border-bottom: 5px solid ${({ theme }) => theme.screenBackground};
+`;
+
+const PageNav = styled.div`
+  padding: 15px 0 15px 0;
+  display: flex;
+  position: sticky;
+  top: 55px;
+  width: 100%;
+  background-color: white;
+  z-index: 2;
+  border-bottom: 5px solid ${({ theme }) => theme.screenBackground};
+`;
+
 //  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.4);
 
 const Screen = ({ children }) => {
@@ -37,13 +63,40 @@ const Screen = ({ children }) => {
   const toggleTheme3 = () => {
     setTheme(Theme3);
   };
+
   return (<ThemeProvider theme={theme}>
           <StyledScreen>{children}
-          <div style={{ position: 'fixed', top: '0', width: '100px', height: '50px', backgroundColor: 'green'}}>
+          <ThemeNav>{children}
           <button onClick={toggleTheme1} id='T1'></button>
           <button onClick={toggleTheme2} id='T2'></button>
           <button onClick={toggleTheme3} id='T3'></button>
-          </div>
+          </ThemeNav>
+
+          
+            <Router>
+              {/* Navigation Bar */}
+              <PageNav>{children}
+                  <div style={{ width: '100%', textAlign: 'center'}}>
+                    <Link to="/">Home</Link>
+                  </div>
+                  <div style={{ width: '100%', textAlign: 'center'}}>
+                    <Link to="/education">Education</Link>
+                  </div>
+                  <div style={{ width: '100%', textAlign: 'center'}}>
+                    <Link to="/portfolio">Portfolio</Link>
+                  </div>
+              </PageNav>
+
+              <div style={{top: '0px', padding: '30px', width: 'auto', height: 'auto', backgroundColor: 'beige' }}>
+              {/* Define Routes */}
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/education" element={<Education />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+              </Routes>
+            </div>
+          </Router>
+      
           </StyledScreen>
           </ThemeProvider>);
 };
